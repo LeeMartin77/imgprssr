53e36f67ee19ff91d4770c8e41407a9fbc66c85d
Lee Martin <hello@leejohnmartin.co.uk>
2022-12-28 17:21:13 +0000

    Update: Still slow - strip temp file read/writes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-
-use std::fs::{File, DirBuilder};
-use std::io::Read;
+use std::io::{Read, Cursor};
 //use std::io::prelude::*;
 use std::path::Path;
 //use std::error::Error;
 
 use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
-use uuid::Uuid;
 
 async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let target_path = req.uri().path();
@@ -19,18 +16,14 @@ async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let img_res = image::open(path);
     match img_res {
         Ok(img) => {
-            let temp_dir = format!("/tmp/{}", Uuid::new_v4());
-            let temp_path =  format!("{}{}", temp_dir, target_path);
-            println!("{}",temp_path);
-            DirBuilder::new().create(temp_dir).unwrap();
-            File::create(&temp_path).unwrap();
-            img.save(&temp_path).unwrap();
-            let mut file = File::open(&temp_path).unwrap();
-            let mut buff = vec![];
-            file.read_to_end(&mut buff).unwrap();
+            let mut buffer = Cursor::new(Vec::new());
+            img.write_to(&mut buffer, image::ImageFormat::Png).unwrap();
+            let mut out = Vec::new();
+            buffer.set_position(0);
+            buffer.read_to_end(&mut out).unwrap();
             Ok(Response::builder()
             .status(StatusCode::OK)
-            .body(buff.into()).unwrap())
+            .body(out.into()).unwrap())
         },
         Err(_) => {
             Ok(Response::builder()
