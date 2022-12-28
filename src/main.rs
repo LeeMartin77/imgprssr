d1df50684efa97aac3a075366ff941cafcc1e501
Lee Martin <hello@leejohnmartin.co.uk>
2022-12-28 21:35:37 +0000

    Added: Support for width param

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
@@ -12,6 +13,16 @@ use hyper::service::{make_service_fn, service_fn};
 
 async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let target_path = req.uri().path();
+    let query_string = req.uri().query().unwrap_or("");
+    let query_parts = query_string.split("&");
+    let mut params: HashMap<&str, &str> = HashMap::new();
+
+    for q in query_parts {
+        let mut prts = q.split("=").into_iter();
+        let key = prts.next().unwrap();
+        let val = prts.next().unwrap_or("true");
+        params.insert(key, val);
+    }
     let raw_path = format!("./images{}", target_path);
     let path = Path::new(&raw_path);
     let now = Instant::now();
@@ -21,8 +32,22 @@ async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     println!("Postload: {:?}", now.elapsed());
 
     match img_res {
-        Ok(img) => {
+        Ok(mut img) => {
             let mut buffer = Cursor::new(Vec::new());
+
+            let width = params.get("width");
+
+            if let Some(w) = width {
+                let wir: Result<u32, _> = w.parse();
+                let source_width = img.width();
+                if let Ok(width_int) = wir  { 
+                    if source_width != width_int {
+                        let width_factor = source_width as f32 / width_int as f32;
+                        let nheight = img.height() as f32 * width_factor;
+                        img = img.resize(width_int, nheight as u32, image::imageops::FilterType::Nearest);
+                    }
+                }
+            }
 
             println!("Prewrite: {:?}", now.elapsed());
             img.write_to(&mut buffer, image::ImageFormat::from_path(path).unwrap()).unwrap();
