705fd1755dda991902ad4cbdbc6538518110e46c
Lee Martin <hello@leejohnmartin.co.uk>
2022-12-28 14:04:49 +0000

    Added: Dog slow, processes image.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 
 
-use std::fs::File;
+use std::fs::{File, DirBuilder};
 use std::io::Read;
 //use std::io::prelude::*;
 use std::path::Path;
@@ -10,18 +10,27 @@ use std::path::Path;
 
 use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
+use uuid::Uuid;
 
 async fn hello_world(req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let raw_path = format!("./images{}", req.uri().path());
+    let target_path = req.uri().path();
+    let raw_path = format!("./images{}", target_path);
     let path = Path::new(&raw_path);
-    let file_res = File::open(&path);
-    match file_res {
-        Ok(mut file) => {
+    let img_res = image::open(path);
+    match img_res {
+        Ok(img) => {
+            let temp_dir = format!("/tmp/{}", Uuid::new_v4());
+            let temp_path =  format!("{}{}", temp_dir, target_path);
+            println!("{}",temp_path);
+            DirBuilder::new().create(temp_dir).unwrap();
+            File::create(&temp_path).unwrap();
+            img.save(&temp_path).unwrap();
+            let mut file = File::open(&temp_path).unwrap();
             let mut buff = vec![];
             file.read_to_end(&mut buff).unwrap();
             Ok(Response::builder()
             .status(StatusCode::OK)
-            .body(buff.into()).unwrap().into())
+            .body(buff.into()).unwrap())
         },
         Err(_) => {
             Ok(Response::builder()
