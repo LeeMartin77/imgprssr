0053d52af4718f8683c8a5ccec7407b6a2ec1c87
Lee Martin <hello@leejohnmartin.co.uk>
2022-12-29 16:24:21 +0000

    Update: Breaking out validation to own function

--- src/main.rs
+++ src/main.rs
@@ -8,34 +8,39 @@ mod parameters;
 mod process;
 
 async fn handle_image_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let validated = validate_request(req);
+    match validated {
+        Ok((img, img_format, params)) => Ok(Response::builder()
+            .status(StatusCode::OK)
+            .body(process::process_image_to_buffer(img, img_format, params).into()).unwrap()),
+        Err(err_res) => Ok(err_res),
+    }
+}
+
+fn validate_request(req: Request<Body>) -> Result<(image::DynamicImage, image::ImageFormat, parameters::ImageParameters), Response<Body>> {
     let target_path = req.uri().path();
     let params_res: Result<parameters::ImageParameters, _> = req.uri().query().unwrap_or("").parse();
     if params_res.is_err() {
-        return Ok(Response::builder()
-        .status(StatusCode::BAD_REQUEST)
-        .body("Bad Request".into()).unwrap().into())
+        return Err(Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body("Bad Request".into()).unwrap().into())
     }
     let params = params_res.unwrap();
     
     let raw_path = format!("./images{}", target_path);
     let path = Path::new(&raw_path);
-    
     let img_res = image::open(path);
-    
     match img_res {
         Ok(img) => {
             let img_format = image::ImageFormat::from_path(path).unwrap();
-            Ok(Response::builder()
-                .status(StatusCode::OK)
-                .body(process::process_image_to_buffer(img, img_format, params).into()).unwrap())
+            Ok((img, img_format, params))
         },
         Err(_) => {
-            Ok(Response::builder()
+            Err(Response::builder()
                 .status(StatusCode::NOT_FOUND)
                 .body("Not Found".into()).unwrap().into())
         }
     }
-    
 }
 
 #[tokio::main]
