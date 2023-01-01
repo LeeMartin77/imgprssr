3b8814cf07d76a68eaf475e35e36a2c67489cc01
Lee Martin <hello@leejohnmartin.co.uk>
2023-01-01 17:33:27 +0000

    Added: Standalone height handling

--- src/process.rs
+++ src/process.rs
@@ -18,10 +18,18 @@ pub fn process_image(settings: &ImgprssrConfig, mut img: DynamicImage, params: c
   let oversize_handling = if let Some(os) = params.oversized_handling { os } else { settings.default_oversize_handling };
   if let Some(w) = params.width {
     let source_width = img.width();
-    if oversize_handling == OversizedImageHandling::Clamp && source_width > w {
+    if params.height.is_none() && oversize_handling == OversizedImageHandling::Clamp && source_width > w {
       let width_factor = source_width as f32 / w as f32;
       let nheight = img.height() as f32 * width_factor;
       img = img.resize(w, nheight as u32, scaling_filter);
+    }
+  }
+  if let Some(h) = params.height {
+    let source_height = img.height();
+    if params.width.is_none() && oversize_handling == OversizedImageHandling::Clamp && source_height > h {
+      let height_factor = source_height as f32 / h as f32;
+      let nwidth = img.width() as f32 * height_factor;
+      img = img.resize(nwidth as u32, h, scaling_filter);
     }
   }
   img
@@ -50,13 +58,26 @@ mod tests {
   #[test]
   fn sets_width_with_matching_aspect_height() {
     let source_size = [1200_u32, 600];
-    let cases = [[200_u32, 100], [300_u32, 150], [600_u32, 300]];
+    let cases = [[123_u32, 61], [200_u32, 100], [300_u32, 150], [600_u32, 300]];
     for case in cases {
       let img = image::DynamicImage::new_rgb8(source_size[0], source_size[1]);
       let params = ImageParameters { width: Some(case[0]), height: None, scaling_filter: None, oversized_handling: None };
       let processed = process_image(&ImgprssrConfig::default(), img, params);
       assert_eq!(processed.width(), case[0]);
       assert_eq!(processed.height(), case[1]);
+    }
+  }
+
+  #[test]
+  fn sets_height_with_matching_aspect_width() {
+    let source_size = [1200_u32, 600];
+    let cases = [[123_u32, 246], [200_u32, 400], [150_u32, 300], [300_u32, 600]];
+    for case in cases {
+      let img = image::DynamicImage::new_rgb8(source_size[0], source_size[1]);
+      let params = ImageParameters { width: None, height: Some(case[0]), scaling_filter: None, oversized_handling: None };
+      let processed = process_image(&ImgprssrConfig::default(), img, params);
+      assert_eq!(processed.height(), case[0]);
+      assert_eq!(processed.width(), case[1]);
     }
   }
 }
