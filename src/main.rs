e4dc6736553155c8a85feb39872df791db42bf4d
Lee Martin <hello@leejohnmartin.co.uk>
2023-01-03 22:03:26 +0000

    Added: Configurable address, port and directory

--- src/main.rs
+++ src/main.rs
@@ -24,14 +24,45 @@ async fn handle_image_request(settings: imgprssr_core::appconfig::ImgprssrConfig
 }
 
 
-pub fn generate_app_config() -> Result<imgprssr_core::appconfig::ImgprssrConfig, imgprssr_core::appconfig::ImgprssrConfigErr> {
-    imgprssr_core::appconfig::from_hashmap(Config::builder()
-          // ENV Variables are IMGPRSSR_SOMETHING == something
-          .add_source(config::Environment::with_prefix("IMGPRSSR"))
-          .build()
-          .unwrap()
-          .try_deserialize::<HashMap<String, String>>()
-          .unwrap())
+pub fn generate_app_config() -> Result<(([u8; 4], u16), imgprssr_core::appconfig::ImgprssrConfig), imgprssr_core::appconfig::ImgprssrConfigErr> {
+    let raw_hashmap = Config::builder()
+        // ENV Variables are IMGPRSSR_SOMETHING == something
+        .add_source(config::Environment::with_prefix("IMGPRSSR"))
+        .build()
+        .unwrap()
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap();
+
+    let mut address = [0, 0, 0, 0];
+    let mut port = 3000;
+
+    if let Some(config_address) = raw_hashmap.get("address") {
+        let split = config_address.split(".");
+        let mut parsed = split.map(|a| a.parse::<u8>()).into_iter();
+        if parsed.clone().count() != 4 && !parsed.clone().all(|res| res.is_ok()){
+            return Err(imgprssr_core::appconfig::ImgprssrConfigErr::InvalidValues(vec!["address".to_owned()]))
+        }
+        let mut i = 0;
+        while let Some(part) = parsed.next() {
+            address[i] = part.unwrap();
+            i += 1;
+        }
+    }
+
+    if let Some(config_port) = raw_hashmap.get("port") {
+        if let Ok(prsd_port) = config_port.parse::<u16>() {
+            port = prsd_port;
+        } else {
+            return Err(imgprssr_core::appconfig::ImgprssrConfigErr::InvalidValues(vec!["port".to_owned()]))
+        }
+    }
+    
+    let app_config_res = imgprssr_core::appconfig::from_hashmap(raw_hashmap);
+
+    match app_config_res {
+        Ok(app_config) => Ok(((address, port), app_config)),
+        Err(err) => Err(err),
+    }
   }
 
 #[tokio::main]
@@ -42,9 +73,11 @@ async fn main() -> Result<(), std::io::Error> {
         SIGQUIT,
     ])?;
 
-    let settings: imgprssr_core::appconfig::ImgprssrConfig = generate_app_config().unwrap();
+    let ((bind, port), settings) = generate_app_config().unwrap();
     
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let addr = SocketAddr::from((bind.clone(), port.clone()));
+
+    println!("imgprssr sourcing images from {}", settings.image_source);
 
     let make_svc = make_service_fn(move |_conn| {
         let settings = settings.clone();
@@ -68,6 +101,8 @@ async fn main() -> Result<(), std::io::Error> {
             }
         });
 
+    println!("imgprssr running on {}.{}.{}.{}:{}", bind[0], bind[1], bind[2], bind[3], port);
+    
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
     }
